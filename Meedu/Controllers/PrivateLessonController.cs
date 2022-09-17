9fb30d3b4f93e4cd6e1488ea53a7793fca27b621
Kamil Chrobok <kchrobok.contact@gmail.com>
2022-09-17 18:40:29 +0200

    Add private lesson controller and service with add ep

---
+++ Meedu/Controllers/PrivateLessonController.cs
@@ -0,0 +1,27 @@
+﻿using Meedu.Models;
+using Meedu.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Meedu.Controllers
+{
+    [Authorize]
+    [Route("api/lessons")]
+    [ApiController]
+    public class PrivateLessonController : ControllerBase
+    {
+        private readonly IPrivateLessonService privateLessonService;
+
+        public PrivateLessonController(IPrivateLessonService privateLessonService)
+        {
+            this.privateLessonService = privateLessonService;
+        }
+
+        [HttpPost("add")]
+        public async Task<ActionResult> AddPrivateLesson([FromBody] PrivateLessonOfferDto dto)
+        {
+            await privateLessonService.AddPrivateLesson(dto);
+            return Ok();
+        }
+    }
+}
---
+++ Meedu/Services/PrivateLessonService.cs
@@ -0,0 +1,59 @@
+﻿using Meedu.Entities;
+using Meedu.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Meedu.Services
+{
+    public interface IPrivateLessonService
+    {
+        Task AddPrivateLesson(PrivateLessonOfferDto dto);
+    }
+
+    public class PrivateLessonService : IPrivateLessonService
+    {
+        private readonly MeeduDbContext dbContext;
+        private readonly ILogger<PrivateLessonService> logger;
+        private readonly IUserContextService userContextService;
+
+        public PrivateLessonService(MeeduDbContext dbContext, ILogger<PrivateLessonService> logger, IUserContextService userContextService)
+        {
+            this.dbContext = dbContext;
+            this.logger = logger;
+            this.userContextService = userContextService;
+        }
+
+        public async Task AddPrivateLesson(PrivateLessonOfferDto dto)
+        {
+            var userID = userContextService.GetUserId;
+            var user = await dbContext.Users.FirstOrDefaultAsync(u => u.Id == userID);
+
+            var subject = await dbContext.Subjects
+                .FirstOrDefaultAsync(s => s.Name.Contains(dto.Subject.Name));
+
+            if (subject == null)
+            {
+                subject = new Subject() { Name = dto.Subject.Name };
+                dbContext.Subjects.Add(subject);
+            }
+
+            if (user != null)
+            {
+                var newLesson = new PrivateLessonOffer()
+                {
+                    City = dto.City,
+                    Description = dto.Description,
+                    LessonTitle = dto.LessonTitle,
+                    OnlineLessonsPossible = dto.isOnline,
+                    Place = dto.Place,
+                    Price = dto.Price,
+                    TeachingRange = dto.TeachingRange,
+                    CreatedBy = user,
+                    Subject = subject
+                };
+
+                dbContext.PrivateLessonOffers.Add(newLesson);
+            }
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
