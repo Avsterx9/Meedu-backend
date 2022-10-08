6a0d5c2d7d5b35b334647df969a7488035489c31
Kamil Chrobok <kchrobok.contact@gmail.com>
2022-10-08 18:38:03 +0200

    Add subject controller

---
+++ Meedu/Controllers/SubjectController.cs
@@ -0,0 +1,18 @@
+﻿using Meedu.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Meedu.Controllers
+{
+    [Route("api/subjects")]
+    [ApiController]
+    public class SubjectController : ControllerBase
+    {
+        //private readonly ISubjectService subjectService;
+
+        [HttpGet("list")]
+        public async Task<ActionResult> AddPrivateLesson()
+        {
+            return Ok();
+        }
+    }
+}
