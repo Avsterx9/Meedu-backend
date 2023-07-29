9eba2a967b4444a8333bfd6020e9f45b9564b4b7
Kamil Chrobok <kchrobok.contact@gmail.com>
2023-07-29 20:36:10 +0200

    Adjust namespace to reduce nesting

--- Meedu/Controllers/AccountController.cs
+++ Meedu/Controllers/AccountController.cs
@@ -4,55 +4,54 @@ using Meedu.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Meedu.Controllers
+namespace Meedu.Controllers;
+
+[Route("api/accounts")]
+[ApiController]
+public class AccountController : ControllerBase
 {
-    [Route("api/accounts")]
-    [ApiController]
-    public class AccountController : ControllerBase
+    private readonly IAccountService _accountService;
+
+    public AccountController(IAccountService accountService)
     {
-        private readonly IAccountService _accountService;
+        _accountService = accountService;
+    }
 
-        public AccountController(IAccountService accountService)
-        {
-            _accountService = accountService;
-        }
+    [HttpPost("register")]
+    public async Task<ActionResult> RegisterUser([FromBody] RegisterUserDto registerUserDto)
+    {
+        await _accountService.RegisterUserAsync(registerUserDto);
+        return Ok();
+    }
 
-        [HttpPost("register")]
-        public async Task<ActionResult> RegisterUser([FromBody] RegisterUserDto registerUserDto)
-        {
-            await _accountService.RegisterUserAsync(registerUserDto);
-            return Ok();
-        }
+    [HttpPost("login")]
+    public async Task<ActionResult> LoginAsync([FromBody] LoginUserDto loginDto)
+    {
+        string token = await _accountService.GenerateJwtTokenAsync(loginDto);
+        return Ok(token);
+    }
 
-        [HttpPost("login")]
-        public async Task<ActionResult> LoginAsync([FromBody] LoginUserDto loginDto)
-        {
-            string token = await _accountService.GenerateJwtTokenAsync(loginDto);
-            return Ok(token);
-        }
-
-        [HttpGet("getUserInfo")]
-        [Authorize]
-        public async Task<ActionResult<UserInfoDto>> GetUserInfoAsync()
-        {
-            return Ok(await _accountService.GetUserInfoAsync());
-        }
+    [HttpGet("getUserInfo")]
+    [Authorize]
+    public async Task<ActionResult<UserInfoDto>> GetUserInfoAsync()
+    {
+        return Ok(await _accountService.GetUserInfoAsync());
+    }
 
 
-        [HttpPut("updateUserData")]
-        [Authorize]
-        public async Task<ActionResult<UserInfoDto>> UpdateUserDataAsync(UpdateUserDataRequest request)
-        {
-            await _accountService.UpdateUserDataAsync(request);
-            return Ok();
-        }
+    [HttpPut("updateUserData")]
+    [Authorize]
+    public async Task<ActionResult<UserInfoDto>> UpdateUserDataAsync(UpdateUserDataRequest request)
+    {
+        await _accountService.UpdateUserDataAsync(request);
+        return Ok();
+    }
 
-        [HttpPost("setUserImage")]
-        [Authorize]
-        public async Task<ActionResult> SetUserImageAsync(IFormFile file)
-        {
-            await _accountService.SetUserImageAsync(file);
-            return Ok();
-        }
+    [HttpPost("setUserImage")]
+    [Authorize]
+    public async Task<ActionResult> SetUserImageAsync(IFormFile file)
+    {
+        await _accountService.SetUserImageAsync(file);
+        return Ok();
     }
 }
--- Meedu/Services/AccountService.cs
+++ Meedu/Services/AccountService.cs
@@ -13,138 +13,137 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 
-namespace Meedu.Services
+namespace Meedu.Services;
+
+public class AccountService : IAccountService
 {
-    public class AccountService : IAccountService
+    private readonly MeeduDbContext _context;
+    private readonly IPasswordHasher<User> _passwordHasher;
+    private readonly ILogger<AccountService> _logger;
+    private readonly AuthSettings _authSettings;
+    private readonly IUserContextService _userContextService;
+    private readonly IMapper _mapper;
+
+    public AccountService(MeeduDbContext dbContext, 
+        IPasswordHasher<User> passwordHasher, 
+        ILogger<AccountService> logger, 
+        AuthSettings authSettings, 
+        IUserContextService userContextService,
+        IMapper mapper)
     {
-        private readonly MeeduDbContext _context;
-        private readonly IPasswordHasher<User> _passwordHasher;
-        private readonly ILogger<AccountService> _logger;
-        private readonly AuthSettings _authSettings;
-        private readonly IUserContextService _userContextService;
-        private readonly IMapper _mapper;
+        _context = dbContext;
+        _passwordHasher = passwordHasher;
+        _logger = logger;
+        _authSettings = authSettings;
+        _userContextService = userContextService;
+        _mapper = mapper;
+    }
 
-        public AccountService(MeeduDbContext dbContext, 
-            IPasswordHasher<User> passwordHasher, 
-            ILogger<AccountService> logger, 
-            AuthSettings authSettings, 
-            IUserContextService userContextService,
-            IMapper mapper)
+    public async Task RegisterUserAsync(RegisterUserDto dto)
+    {
+        var newUser = _mapper.Map<User>(dto);
+        var hashedPassword = _passwordHasher.HashPassword(newUser, dto.Password);
+        newUser.PasswordHash = hashedPassword;
+
+        await _context.Users.AddAsync(newUser);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<string> GenerateJwtTokenAsync(LoginUserDto loginDto)
+    {
+        var user = await _context.Users
+            .Include(x => x.Role)
+            .FirstOrDefaultAsync(x => x.Email == loginDto.Email)
+            ?? throw new BadRequestException(ExceptionMessages.InvalidUsernameOrPassword);
+
+        var result = _passwordHasher.VerifyHashedPassword(user, user.PasswordHash, loginDto.Password);
+
+        if (result == PasswordVerificationResult.Failed)
+            throw new BadRequestException(ExceptionMessages.InvalidUsernameOrPassword);
+
+        var claims = new List<Claim>()
         {
-            _context = dbContext;
-            _passwordHasher = passwordHasher;
-            _logger = logger;
-            _authSettings = authSettings;
-            _userContextService = userContextService;
-            _mapper = mapper;
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new Claim(ClaimTypes.Name, $"{user.FirstName} {user.LastName}"),
+            new Claim(ClaimTypes.Name, $"{user.Role.Name}"),
+            new Claim("DateOfBirth", user.DateOfBirth.Value.ToString("dd-MM-yyyy"))
+        };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_authSettings.JwtKey));
+        var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+        var expires = DateTime.Now.AddDays(_authSettings.JwtExpireDays);
+
+        var token = new JwtSecurityToken(_authSettings.JwtIssuer,
+            _authSettings.JwtIssuer,
+            claims,
+            expires: expires,
+            signingCredentials: cred);
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        return tokenHandler.WriteToken(token);
+    }
+
+    public async Task<UserInfoDto> GetUserInfoAsync()
+    {
+        var userId = _userContextService.GetUserIdFromToken();
+
+        var user = await _context.Users
+            .Include(x => x.Image)
+            .FirstOrDefaultAsync(u => u.Id == userId)
+            ?? throw new NotFoundException(ExceptionMessages.UserNotFound);
+
+        return _mapper.Map<UserInfoDto>(user);
+    }
+
+    public async Task<UserInfoDto> UpdateUserDataAsync(UpdateUserDataRequest request)
+    {
+        var userId = _userContextService.GetUserIdFromToken();
+
+        var user = await _context.Users
+            .Include(x => x.Image)
+            .FirstOrDefaultAsync(x => x.Id == userId)
+            ?? throw new NotFoundException(ExceptionMessages.UserNotFound);
+
+        user.PhoneNumber = request.PhoneNumber;
+        user.FirstName = request.FirstName;
+        user.LastName = request.LastName;
+        user.DateOfBirth = request.DateOfBirth;
+
+        await _context.SaveChangesAsync();
+
+        return _mapper.Map<UserInfoDto>(user);
+    }
+
+    public async Task SetUserImageAsync(IFormFile file)
+    {
+        var userId = _userContextService.GetUserIdFromToken();
+
+        var user = await _context.Users
+            .Include(x => x.Image)
+            .FirstOrDefaultAsync(x => x.Id == userId)
+            ?? throw new NotFoundException(ExceptionMessages.UserNotFound);
+
+        if (file == null || file.Length == 0)
+            throw new BadRequestException(ExceptionMessages.FileIsEmpty);
+
+        byte[] data;
+
+        using (var stream = new MemoryStream())
+        {
+            Bitmap bitmap = new Bitmap(file.OpenReadStream());
+            bitmap.Save(stream, ImageFormat.Jpeg);
+            data = stream.ToArray();
         }
 
-        public async Task RegisterUserAsync(RegisterUserDto dto)
+        var image = new Meedu.Entities.Image
         {
-            var newUser = _mapper.Map<User>(dto);
-            var hashedPassword = _passwordHasher.HashPassword(newUser, dto.Password);
-            newUser.PasswordHash = hashedPassword;
+            ContentType = file.ContentType,
+            Data = data
+        };
 
-            await _context.Users.AddAsync(newUser);
-            await _context.SaveChangesAsync();
-        }
+        user.Image = image;
 
-        public async Task<string> GenerateJwtTokenAsync(LoginUserDto loginDto)
-        {
-            var user = await _context.Users
-                .Include(x => x.Role)
-                .FirstOrDefaultAsync(x => x.Email == loginDto.Email)
-                ?? throw new BadRequestException(ExceptionMessages.InvalidUsernameOrPassword);
-
-            var result = _passwordHasher.VerifyHashedPassword(user, user.PasswordHash, loginDto.Password);
-
-            if (result == PasswordVerificationResult.Failed)
-                throw new BadRequestException(ExceptionMessages.InvalidUsernameOrPassword);
-
-            var claims = new List<Claim>()
-            {
-                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
-                new Claim(ClaimTypes.Name, $"{user.FirstName} {user.LastName}"),
-                new Claim(ClaimTypes.Name, $"{user.Role.Name}"),
-                new Claim("DateOfBirth", user.DateOfBirth.Value.ToString("dd-MM-yyyy"))
-            };
-
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_authSettings.JwtKey));
-            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-            var expires = DateTime.Now.AddDays(_authSettings.JwtExpireDays);
-
-            var token = new JwtSecurityToken(_authSettings.JwtIssuer,
-                _authSettings.JwtIssuer,
-                claims,
-                expires: expires,
-                signingCredentials: cred);
-
-            var tokenHandler = new JwtSecurityTokenHandler();
-            return tokenHandler.WriteToken(token);
-        }
-
-        public async Task<UserInfoDto> GetUserInfoAsync()
-        {
-            var userId = _userContextService.GetUserIdFromToken();
-
-            var user = await _context.Users
-                .Include(x => x.Image)
-                .FirstOrDefaultAsync(u => u.Id == userId)
-                ?? throw new NotFoundException(ExceptionMessages.UserNotFound);
-
-            return _mapper.Map<UserInfoDto>(user);
-        }
-
-        public async Task<UserInfoDto> UpdateUserDataAsync(UpdateUserDataRequest request)
-        {
-            var userId = _userContextService.GetUserIdFromToken();
-
-            var user = await _context.Users
-                .Include(x => x.Image)
-                .FirstOrDefaultAsync(x => x.Id == userId)
-                ?? throw new NotFoundException(ExceptionMessages.UserNotFound);
-
-            user.PhoneNumber = request.PhoneNumber;
-            user.FirstName = request.FirstName;
-            user.LastName = request.LastName;
-            user.DateOfBirth = request.DateOfBirth;
-
-            await _context.SaveChangesAsync();
-
-            return _mapper.Map<UserInfoDto>(user);
-        }
-
-        public async Task SetUserImageAsync(IFormFile file)
-        {
-            var userId = _userContextService.GetUserIdFromToken();
-
-            var user = await _context.Users
-                .Include(x => x.Image)
-                .FirstOrDefaultAsync(x => x.Id == userId)
-                ?? throw new NotFoundException(ExceptionMessages.UserNotFound);
-
-            if (file == null || file.Length == 0)
-                throw new BadRequestException(ExceptionMessages.FileIsEmpty);
-
-            byte[] data;
-
-            using (var stream = new MemoryStream())
-            {
-                Bitmap bitmap = new Bitmap(file.OpenReadStream());
-                bitmap.Save(stream, ImageFormat.Jpeg);
-                data = stream.ToArray();
-            }
-
-            var image = new Meedu.Entities.Image
-            {
-                ContentType = file.ContentType,
-                Data = data
-            };
-
-            user.Image = image;
-
-            await _context.Images.AddAsync(image);
-            await _context.SaveChangesAsync();
-        }
+        await _context.Images.AddAsync(image);
+        await _context.SaveChangesAsync();
     }
 }
